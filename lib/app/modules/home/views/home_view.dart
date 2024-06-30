45e3cc1f9aea788313ce0119838440460fc5eee2
FIKRI140702 <nugrahaeni8@gmail.com>
2024-06-30 14:19:10 +0700

    Detail Presensi

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:presence/app/controllers/page_index_controller.dart';
+import 'package:presence/app/routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -148,7 +149,7 @@ class HomeView extends GetView<HomeController> {
                       ),
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () => Get.toNamed(Routes.ALL_PRESENSI),
                       child: const Text("See More"),
                     ),
                   ],
@@ -159,41 +160,53 @@ class HomeView extends GetView<HomeController> {
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: 5,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 20),
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Material(
                         color: Colors.grey[200],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Masuk",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                        borderRadius: BorderRadius.circular(20),
+                        child: InkWell(
+                          onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI),
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Masuk",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "${DateFormat.yMMMEd().format(DateTime.now())}",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              Text(
-                                "${DateFormat.yMMMEd().format(DateTime.now())}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                                Text(
+                                    "${DateFormat.jms().format(DateTime.now())}"),
+                                SizedBox(height: 10),
+                                Text(
+                                  "Keluar",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
                                 ),
-                              ),
-                            ],
+                                Text(
+                                    "${DateFormat.jms().format(DateTime.now())}"),
+                              ],
+                            ),
                           ),
-                          Text("${DateFormat.jms().format(DateTime.now())}"),
-                          SizedBox(height: 10),
-                          Text(
-                            "Keluar",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text("${DateFormat.jms().format(DateTime.now())}"),
-                        ],
+                        ),
                       ),
                     );
                   },
